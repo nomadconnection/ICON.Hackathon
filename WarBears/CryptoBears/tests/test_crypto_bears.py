16aa0fac7722872cac9e1d1dd873bc5eeec71dde
Life4honor <choejy0329@gmail.com>
2018-12-13 00:07:33 +0900

    Refactor Test Code for CryptoBears.py

--- WarBears/CryptoBears/tests/test_crypto_bears.py
+++ WarBears/CryptoBears/tests/test_crypto_bears.py
@@ -4,6 +4,7 @@ from iconsdk.builder.call_builder import CallBuilder
 from iconsdk.builder.transaction_builder import DeployTransactionBuilder, CallTransactionBuilder
 from iconsdk.libs.in_memory_zip import gen_deploy_data_content
 from iconsdk.signed_transaction import SignedTransaction
+from iconservice.base.address import Address
 from tbears.libs.icon_integrate_test import IconIntegrateTestBase, SCORE_INSTALL_ADDRESS
 
 DIR_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -16,18 +17,12 @@ class TestCryptoBears(IconIntegrateTestBase):
 
     def setUp(self):
         super().setUp()
-
         self.icon_service = None
-        # if you want to send request to network, uncomment next line and set self.TEST_HTTP_ENDPOINT_URI_V3
-        # self.icon_service = IconService(HTTPProvider(self.TEST_HTTP_ENDPOINT_URI_V3))
-
-        # install SCORE
         self._score_address = self._deploy_score(score_path=self.SCORE_PROJECT)['scoreAddress']
         self._bear_factory = \
             self._deploy_score(score_path=self.BEAR_FACTORY, params={"_score": self._score_address})['scoreAddress']
 
     def _deploy_score(self, score_path: str, to: str = SCORE_INSTALL_ADDRESS, params: dict = None) -> dict:
-        # Generates an instance of transaction for deploying SCORE.
         transaction = DeployTransactionBuilder() \
             .from_(self._test1.get_address()) \
             .to(to) \
@@ -39,10 +34,7 @@ class TestCryptoBears(IconIntegrateTestBase):
             .params(params) \
             .build()
 
-        # Returns the signed transaction object having a signature
         signed_transaction = SignedTransaction(transaction, self._test1)
-
-        # process the transaction in local
         tx_result = self.process_transaction(signed_transaction, self.icon_service)
 
         self.assertTrue('status' in tx_result)
@@ -52,150 +44,134 @@ class TestCryptoBears(IconIntegrateTestBase):
         return tx_result
 
     def test_score_update(self):
-        # update SCORE
         tx_result = self._deploy_score(score_path=self.SCORE_PROJECT, to=self._score_address)
 
         self.assertEqual(self._score_address, tx_result['scoreAddress'])
 
     def test_call_name(self):
-        # Generates a call instance using the CallBuilder
-        call = CallBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .method("name") \
-            .build()
+        call = self._icx_call(_method="name")
+        self._name = self.process_call(call, self.icon_service)
+        print("Name : " + self._name)
 
-        # Sends the call request
-        response = self.process_call(call, self.icon_service)
-
-        self.assertEqual("CryptoBears", response)
+        self.assertEqual("CryptoBears", self._name)
 
     def test_call_symbol(self):
-        call = CallBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .method("symbol") \
-            .build()
+        call = self._icx_call(_method="symbol")
+        self._symbol = self.process_call(call, self.icon_service)
+        print("Symbol : " + self._symbol)
 
-        response = self.process_call(call, self.icon_service)
-
-        self.assertEqual("CBT", response)
+        self.assertEqual("CBT", self._symbol)
 
     def test_call_balance_of(self):
-        call = CallBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .method("balanceOf") \
-            .params({"_owner": self._test1.get_address()}) \
-            .build()
+        call = self._icx_call(_method="balanceOf", _params={"_owner": self._test1.get_address()})
+        self._balanceBefore = self.process_call(call, self.icon_service)
+        print("balanceBefore : " + str(self._balanceBefore))
 
-        response = self.process_call(call, self.icon_service)
-        self.assertEqual("0x0", response)
+        self.assertEqual("0x0", self._balanceBefore)
 
     def test_call_balance_of_after_create_crypto_bear(self):
-        transaction = CallTransactionBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._bear_factory) \
-            .step_limit(100_000_000_000) \
-            .value(1000000000000000000) \
-            .nid(3) \
-            .method("createCryptoBear") \
-            .build()
-
+        transaction = self._icx_sendTransaction(_to=self._bear_factory, _value=1000000000000000000,
+                                                _method="createCryptoBear")
         signed_transaction = SignedTransaction(transaction, self._test1)
-
         tx_result = self.process_transaction(signed_transaction, self.icon_service)
+
+        call = self._icx_call(_method="balanceOf", _params={"_owner": self._test1.get_address()})
+        self._balanceAfter = self.process_call(call, self.icon_service)
+        print("balanceAfter : " + str(self._balanceAfter))
+
+        self.assertEqual("0x1", self._balanceAfter)
+
+    def test_create_crypto_bear_called_by_ca(self):
+        transaction = self._icx_sendTransaction(_to=self._bear_factory, _value=1000000000000000000,
+                                                _method="createCryptoBear")
+        signed_transaction = SignedTransaction(transaction, self._test1)
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+
         self.assertTrue('status' in tx_result)
         self.assertEqual(1, tx_result['status'])
 
-        call = CallBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .method("balanceOf") \
-            .params({"_owner": self._test1.get_address()}) \
-            .build()
-
-        response = self.process_call(call, self.icon_service)
-        self.assertEqual("0x1", response)
-
-    def test_create_crypto_bear_called_by_ca(self) -> dict:
-        transaction = CallTransactionBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._bear_factory) \
-            .step_limit(100_000_000_000) \
-            .value(1000000000000000000) \
-            .nid(3) \
-            .method("createCryptoBear") \
-            .build()
-
+    def test_create_crypto_bear_called_by_eoa(self):
+        print("------ Expecting error msg here ------")
+        transaction = self._icx_sendTransaction(_to=self._score_address, _method="createCryptoBear",
+                                                _params={"_bearDNA": b'12345', "_address": self._test1.get_address()})
         signed_transaction = SignedTransaction(transaction, self._test1)
-
         tx_result = self.process_transaction(signed_transaction, self.icon_service)
-        self.assertTrue('status' in tx_result)
-        self.assertEqual(1, tx_result['status'])
-        return tx_result
 
-    def test_create_crypto_bear_called_by_eoa(self) -> dict:
-        transaction = CallTransactionBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .step_limit(100_000_000_000) \
-            .nid(3) \
-            .method("createCryptoBear") \
-            .params({"_bearDNA": b'12345', "_address": self._test1.get_address()}) \
-            .build()
-
-        signed_transaction = SignedTransaction(transaction, self._test1)
-
-        tx_result = self.process_transaction(signed_transaction, self.icon_service)
         self.assertTrue('status' in tx_result)
         self.assertEqual(0, tx_result['status'])
+        print("  --------------------------------------")
 
-        return tx_result
-
-    def test_call_get_token_id(self):
-        self.test_create_crypto_bear_called_by_ca()
-
-        call = CallBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .method("getTokenId") \
-            .params({"_address": self._test1.get_address(), "index": "0"}) \
-            .build()
-        response = self.process_call(call, self.icon_service)
-        self._tokenId = str(response)
-        self.assertEqual(66, len(response))
-        self.assertEqual(str, type(response))
-
-    def test_happy_meal(self):
-        self.test_create_crypto_bear_called_by_ca()
-
-        transaction = CallTransactionBuilder() \
-            .from_(self._test1.get_address()) \
-            .to(self._score_address) \
-            .step_limit(100_000_000_000) \
-            .nid(3) \
-            .value(100000000000000000) \
-            .method("happyMeal") \
-            .params({"_index": '0'}) \
-            .build()
-
+    def test_call_get_token_id_after_create_crypto_bear(self):
+        transaction = self._icx_sendTransaction(_to=self._bear_factory, _value=1000000000000000000,
+                                                _method="createCryptoBear")
         signed_transaction = SignedTransaction(transaction, self._test1)
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+        call = self._icx_call(_method="getTokenId", _params={"_address": self._test1.get_address(), "index": "0"})
+
+        self._tokenId = self.process_call(call, self.icon_service)
+        print("tokenId : " + str(self._tokenId))
+
+        self.assertEqual(66, len(self._tokenId))
+        self.assertEqual(str, type(self._tokenId))
+
+    def test_happy_meal_after_create_crypto_bear(self):
+        prerequisite = self._icx_sendTransaction(_to=self._bear_factory, _value=1000000000000000000,
+                                                 _method="createCryptoBear")
+        signed_prerequisite = SignedTransaction(prerequisite, self._test1)
+        self.process_transaction(signed_prerequisite, self.icon_service)
+
+        happymeal_transaction = self._icx_sendTransaction(_to=self._score_address, _value=1000000000000000000,
+                                                          _method="happyMeal", _params={"_index": '0'})
+        signed_transaction = SignedTransaction(happymeal_transaction, self._test1)
         tx_result = self.process_transaction(signed_transaction, self.icon_service)
 
         self.assertTrue('status' in tx_result)
         self.assertEqual(1, tx_result['status'])
 
-    def test_bear_level(self):
-        self.test_call_get_token_id()
+        call_tokenid = self._icx_call(_method="getTokenId",
+                                      _params={"_address": self._test1.get_address(), "index": "0"})
+        self._tokenId = self.process_call(call_tokenid, self.icon_service)
 
+        call_get_bear_level = self._icx_call(_method="getBearLevel", _params={"_tokenId": self._tokenId})
+        self._bearLevel = self.process_call(call_get_bear_level, self.icon_service)
+        print("BearLevelAfterHappyMeal : " + self._bearLevel)
+        self.assertEqual('0x1', self._bearLevel)
+
+    def test_call_get_bear_level(self) -> str:
+        transaction = self._icx_sendTransaction(_to=self._bear_factory, _value=1000000000000000000,
+                                                _method="createCryptoBear")
+        signed_transaction = SignedTransaction(transaction, self._test1)
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+
+        call_tokenid = self._icx_call(_method="getTokenId",
+                                      _params={"_address": self._test1.get_address(), "index": "0"})
+        self._tokenId = self.process_call(call_tokenid, self.icon_service)
+
+        call = self._icx_call(_method="getBearLevel", _params={"_tokenId": self._tokenId})
+
+        self._bearLevel = self.process_call(call, self.icon_service)
+        print("BearLevel : " + self._bearLevel)
+        self.assertEqual('0x0', self._bearLevel)
+
+        return self._bearLevel
+
+    def _icx_call(self, _method: str = "", _params: dict = None):
         call = CallBuilder() \
             .from_(self._test1.get_address()) \
             .to(self._score_address) \
-            .method("getBearLevel") \
-            .params({"_tokenId": self._tokenId}) \
+            .method(_method) \
+            .params(_params) \
             .build()
+        return call
 
-        response = self.process_call(call, self.icon_service)
-        print(response)
-        self.assertEqual('0x0', response)
+    def _icx_sendTransaction(self, _to: Address, _value: int = 0, _method: str = "", _params: dict = None):
+        transaction = CallTransactionBuilder() \
+            .from_(self._test1.get_address()) \
+            .to(_to) \
+            .step_limit(100_000_000_000) \
+            .value(_value) \
+            .nid(3) \
+            .method(_method) \
+            .params(_params) \
+            .build()
+        return transaction
